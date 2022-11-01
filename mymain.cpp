8a3e810f5bb0cb69cb00a372ae712d1c82ed5811
lmarquar <leon.marquardt@gmx.de>
2022-11-01 17:58:40 +0100

    ok i started at it, coded only 2.6 hours, which is enough.

--- mymain.cpp
+++ mymain.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 #include <vector>
+#include "vector.hpp"
  
 int main()
 {
-    // Create a vector containing integers
+    ft::vector<int> v1;
+
+    std::cout << v1.capacity() << std::endl;
+    v1.push_back(10);
+    std::cout << v1.size() << std::endl;
+    
+
+    std::cout << "start of real vector" << std::endl;
+     // Create a vector containing integers
     int arr[4] = {7, 5, 16, 8};
     std::vector<int> v(arr, arr+4);
- 
+    std::vector<int> empty;
+
+    std::cout << empty.capacity() << std::endl;
+    std::cout << v.size() << std::endl;
     // Add two more integers to vector
     v.push_back(25);
     v.push_back(13);
--- vector.hpp
+++ vector.hpp
@@ -3,16 +3,10 @@
 
 # include <iostream>
 # include <string>
+# include <stdexcept>
 
-template<
-	class T
->
-typedef struct s_list
+namespace ft
 {
-	T el;
-	t_list *next;
-} t_list;
-
 template<
 	class T,
 	class Allocator = std::allocator<T>
@@ -21,19 +15,93 @@ class vector
 {
 	public:
 		// Constructors
-		vector();
-		vector(const vector &copy);
-		
+		vector()
+		{
+			arr_capacity = 0;
+			arr_capacity = 0;
+			arr = new T[arr_size];
+		}
+		vector(const vector &copy)
+		{
+			*this = copy;
+		}
+
 		// Destructor
-		~vector();
-		
+		~vector()
+		{
+			std::cout << "destructor of vector called" << std::endl;
+			delete arr;
+		}
+
 		// Operators
-		vector & operator=(const vector &assign);
+		vector & operator=(const vector &assign)
+		{
+			arr_capacity = assign.capacity();
+			arr_size = assign.size();
+			return (*this);
+		}
 		
 		// Getters / Setters
-		
+		inline size_t capacity() const
+		{
+			return arr_capacity;
+		}
+		inline size_t size() const/*  noexcept */
+		{
+			return arr_size;
+		}
+		void push_back(T &el)
+		{
+			if (arr_size == arr_capacity)
+			{
+				T *new_arr;
+				size_t new_arr_capacity;
+
+				if (arr_capacity == 0)
+					new_arr_capacity = 1;
+				else
+					new_arr_capacity = arr_capacity * 2;
+				if (arr_capacity > INT_MAX)
+					throw std::out_of_range("vector size gets too big");
+				new_arr = new int((int)new_arr_capacity);
+				pasteAllInto(new_arr, new_arr_capacity);
+				delete arr;
+				arr = new_arr;
+				arr_capacity = new_arr_capacity;
+			}
+			arr[arr_size++] = el;
+		}
+		void push_back(T el)
+		{
+			if (arr_size == arr_capacity)
+			{
+				T *new_arr;
+				size_t new_arr_capacity;
+
+				new_arr_capacity = arr_capacity * 2;
+				if (arr_capacity > INT_MAX)
+					throw std::out_of_range("vector size gets too big");
+				new_arr = new int((int)new_arr_capacity);
+				pasteAllInto(new_arr, new_arr_capacity);
+				delete arr;
+				arr = new_arr;
+				arr_capacity = new_arr_capacity;
+			}
+			arr[arr_size++] = el;
+		}
+
+
 	private:
-		
+		T		*arr;
+		size_t	arr_capacity;
+		size_t	arr_size;
+
+		void pasteAllInto(T *buf, size_t buf_size)
+		{
+			for (size_t i = 0; i < buf_size; i++)
+				buf[i] = arr[i];
+		}
 };
+}
 
 #endif
