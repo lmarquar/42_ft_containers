23ba0bc48aee4e97c7635a4b3d1ed88298bf12d5
lmarquar <leon.marquardt@gmx.de>
2022-11-03 23:35:13 +0100

    figuring how to use the std::vector and ft::vector with the same funtion

--- mymain.cpp
+++ mymain.cpp
@@ -4,16 +4,39 @@
 
 class A {public: A(){a = 1;} int a;};
 
+/* template <typename Vector>
+void test(Vector t)
+{
+    (void) t;
+    t.push_back(2);
+//    std::cout << t.at(0) << std::endl;
+} */
+
 int main()
 {
     int i;
     ft::vector<int> v1;
 
     i = 10;
-    std::cout << v1.capacity() << std::endl;
-    v1.push_back(1);
-    std::cout << v1.size() << std::endl;
-    
+    test(v1);
+    v1.push_back(2);
+    v1.push_back(3);
+    v1.push_front(1);
+    for(size_t i = 0; i < v1.size(); i++)
+        std::cout << v1.at((int)i) << std::endl;
+    v1.clear();
+    for(size_t i = 0; i < 4; i++)
+        std::cout << v1.at((int)i) << std::endl;
+    std::cout << "back(): " << v1.back() << std::endl;
+    std::cout << "capacity(): " << v1.capacity() << std::endl;
+    std::cout << "size(): " << v1.size() << std::endl;
+
+    std::vector<A> v2;
+    A a;
+    a.a = 2;
+    v2.push_back(a);
+    v2.clear();
+    std::cout << "A: " << v2.back().a << std::endl;
 
     std::cout << "start of real vector" << std::endl;
      // Create a vector containing integers
@@ -21,11 +44,17 @@ int main()
     std::vector<int> v(arr, arr+4);
     std::vector<int> empty;
 
-    std::cout << empty.capacity() << std::endl;
     std::cout << v.size() << std::endl;
     // Add two more integers to vector
     v.push_back(25);
     v.push_back(13);
+
+    std::cout << "v:back1: " << v.back() << std::endl;
+    v.clear();
+    std::cout << "v:back2: " << v.back() << std::endl;
+    std::cout << "v.capa: " << v.capacity() << std::endl;
+    std::cout << "v.size: " << v.size() << std::endl;
+    std::cout << empty.capacity() << std::endl;
  
     // Print out the vector
     std::cout << "v = { ";
--- vector.hpp
+++ vector.hpp
@@ -18,8 +18,8 @@ class vector
 		vector()
 		{
 			arr_capacity = 0;
-			arr_capacity = 0;
-			arr = new T[arr_size];
+			arr_size = 0;
+			arr = new T[arr_capacity];
 		}
 		vector(const vector &copy)
 		{
@@ -57,17 +57,16 @@ class vector
 				T *new_arr;
 				size_t new_arr_capacity;
 
-				new_arr_capacity = arr_capacity * 2;
+				new_arr_capacity = (arr_capacity == 0 ? 1 : (arr_capacity * 2));
 				if (arr_capacity > INT_MAX)
 					throw std::out_of_range("vector size gets too big");
-				new_arr = new int((int)new_arr_capacity);
+				new_arr = new T[new_arr_capacity];
 				pasteAllInto(new_arr, new_arr_capacity);
 				delete arr;
 				arr = new_arr;
 				arr_capacity = new_arr_capacity;
 			}
 			arr[arr_size++] = el;
-			std::cout << "test" << std::endl;
 		}
 		void push_front(T el)
 		{
@@ -76,10 +75,10 @@ class vector
 				T *new_arr;
 				size_t new_arr_capacity;
 
-				new_arr_capacity = arr_capacity * 2;
+				new_arr_capacity = (arr_capacity == 0 ? 1 : (arr_capacity * 2));
 				if (arr_capacity > INT_MAX)
 					throw std::out_of_range("vector size gets too big");
-				new_arr = new int((int)new_arr_capacity);
+				new_arr = new T[new_arr_capacity];
 				pasteAllInto(new_arr, new_arr_capacity);
 				delete arr;
 				arr = new_arr;
@@ -87,10 +86,36 @@ class vector
 			}
 			moveAllToRightByOne();
 			arr[0] = el;
+			arr_size++;
 		}
 		inline T at(int i) const
 		{
 			return (arr[i]);
+		}
+		inline T &front()
+		{
+			if (arr_capacity == 0)
+				throw std::out_of_range("cannot access element of empty vector");
+			return (arr[0]);
+		}
+		inline T &back()
+		{
+			if (arr_capacity == 0)
+				throw std::out_of_range("cannot access element of empty vector");
+			if (arr_size == 0)
+				return (arr[0]);
+			else
+				return (arr[arr_size - 1]);
+		}
+		inline void clear()
+		{
+			delete arr;
+			arr_size = 0;
+			arr = new T[arr_capacity];
+		}
+		T*	data()
+		{
+			return (arr);
 		}
 
 
